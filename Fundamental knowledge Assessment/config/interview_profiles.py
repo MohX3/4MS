170f5e296d85e5ece2bc4b42b324521a74f7033e
Oushim959 <157726528+Oushim959@users.noreply.github.com>
2025-12-18 18:30:09 +0300

    upload

---
+++ Fundamental knowledge Assessment/config/interview_profiles.py
@@ -0,0 +1,9 @@
+﻿INTERVIEWER_PROFILES = {
+    "professional": {
+        "name": "Technical Interviewer",
+        "style": "Thorough evaluation focused on fundamental knowledge",
+        "temperature": 0.3,
+        "scoring_style": "strict",
+        "tone": "formal"
+    }
+}
